9a0a03638d1a9fbd545fcb1b39f1a668805ffc6a
Scott Coates <scoarescoare@gmail.com>
2013-10-18 14:52:36 -0400

    added prepare assets file

---
+++ prepare_assets.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+from gears.environment import Environment
+from gears.finders import FileSystemFinder
+from gears.processors import hexdigest_paths
+
+#hack to exclude 3rd party css from external locations
+hexdigest_paths.URL_RE = re.compile(r"""url\((?!//)(['"]?)\s*(.*?)\s*\1\)""")
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+SOURCE_DIR = os.path.join(ROOT_DIR, '_public')
+DEST_DIR = os.path.join(ROOT_DIR, '_out')
+
+public_assets_config = (
+  lambda path: any(path.endswith(ext) for ext in ('.css', '.js', '.html')),
+)
+
+#defines where files will be emitted
+env = Environment(DEST_DIR, public_assets=public_assets_config)
+env.finders.register(FileSystemFinder([SOURCE_DIR]))
+env.register_defaults()
+
+if __name__ == '__main__':
+  env.save()
+  manifest = env.manifest
+  for k, v in env.manifest.files.items():
+    if k.endswith('html'):
+      path = os.path.join(DEST_DIR, v)
+    else:
+      path = os.path.join(DEST_DIR, k)
+
+    os.remove(path)
